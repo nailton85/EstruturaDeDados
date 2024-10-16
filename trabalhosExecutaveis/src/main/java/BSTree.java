3743dd0832135b7d88b778401a71904045cd28f7
nailton.pereira <pnailton720@gmail.com>
2024-10-16 12:33:40 -0300

    add

---
+++ trabalhosExecutaveis/src/main/java/BSTree.java
@@ -0,0 +1,9 @@
+import java.util.NoSuchElementException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class BSTree {
+    private Node root;
+}
